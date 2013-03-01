eb6504c5adedee374d3d78d4da5eb81f418c18da
foxcarlos <foxcarlos@gmail.com>
2013-03-01 13:09:25 -0430

    new file:   ITIL_User_GuideR11.pdf 	new file:   campos.py 	new file:   ods.py 	new file:   tablas.ods

---
+++ ods.py
@@ -0,0 +1,21 @@
+raw_input('Atutomatico Fecha y Hora actual:')
+
+raw_input('Reportado Por:')
+raw_input('Ingrese el Usuario Afectado:')
+raw_input('Activo de PC:')
+
+raw_input('Ingrese La Localidad u Organizacion:')
+
+raw_input('Tipo de Reporte (incidente ,requerimiento,consulta:')
+raw_input('Ingrese el incidente o Categoria de la falla:')
+raw_input('Summary o resumen de la falla')
+raw_input('Detalle Ampliado de la falla')
+
+raw_input('Ingrese el status del incidente o requerimiento "Open por defecto":')
+raw_input('Prioridad:')
+
+
+
+
+
+
