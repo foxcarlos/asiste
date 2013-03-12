e2cff554d046ffc2b5df0937f2a8f29ff77fffe3
foxcarlos <foxcarlos@gmail.com>
2013-03-12 09:45:09 -0430

    modified:   autocompletar.py

--- autocompletar.py
+++ autocompletar.py
@@ -5,10 +5,10 @@ from PyQt4.QtGui import QApplication, QCompleter, QLineEdit, QStringListModel
 
 if __name__ == "__main__":
     app = QApplication(sys.argv)
-    edit = QLineEdit()
-    lista = ('Carlos', 'Carla', 'Alberto', 'Adrian', 'garcia', 'Diaz')
-    autoCompletar = QCompleter(lista)
-    autoCompletar.setCaseSensitivity(Qt.CaseInsensitive)
-    edit.setCompleter(autoCompletar)
+    edit = QLineEdit()  # Se instancia la clase para Qlineedit
+    lista = ('Carlos', 'Carla', 'Alberto', 'Adrian', 'garcia', 'Diaz')  # Se crea la lista que se mostrara
+    autoCompletar = QCompleter(lista)  # se define la lista 
+    autoCompletar.setCaseSensitivity(Qt.CaseInsensitive)  # se desactiva el case sensitive
+    edit.setCompleter(autoCompletar)  # Se asigna la lista al QlineEdit
     edit.show()
     sys.exit(app.exec_())
